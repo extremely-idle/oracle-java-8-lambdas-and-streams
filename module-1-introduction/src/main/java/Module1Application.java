2737c2156a42b5b595ebca14f42e282bff354d31
Ross Moug <ross.moug@gmail.com>
2018-02-05 19:59:55 +0000

    Initial project setup and stub for module 1.

---
+++ module-1-introduction/src/main/java/Module1Application.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class Module1Application {
+    public static void main(String[] args) {
+        run(Module1Application.class, args);
+    }
+}
